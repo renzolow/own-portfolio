d0cc1c0349331e45721636af4c57090b8d1a6951
renzolow <60082815+renzolow@users.noreply.github.com>
2020-06-24 10:32:40 +0800

    Updates

---
+++ precache-manifest.92dbbffdd71d6fdfb8ec637a176112a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde1c0f79b9baad1fd84578d52dc19a6",
+    "url": "/own-portfolio/index.html"
+  },
+  {
+    "revision": "4f244470be7f0524cb8c",
+    "url": "/own-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e6cf460535c96579ba4e",
+    "url": "/own-portfolio/static/css/main.a886cd05.chunk.css"
+  },
+  {
+    "revision": "4f244470be7f0524cb8c",
+    "url": "/own-portfolio/static/js/2.4e4fa0bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/own-portfolio/static/js/2.4e4fa0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6cf460535c96579ba4e",
+    "url": "/own-portfolio/static/js/main.16b45b59.chunk.js"
+  },
+  {
+    "revision": "c31d2b0fba700e2f4231",
+    "url": "/own-portfolio/static/js/runtime-main.aaa84086.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/own-portfolio/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/own-portfolio/static/media/image.d7265326.png"
+  }
+]);
